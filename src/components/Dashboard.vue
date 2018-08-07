4145f794e00fe4348138dfb87128baa355839bde
Damien Avequin <avequin.da@gmail.com>
2018-08-07 22:44:11 +0200

    ça avance...

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,24 +1,15 @@
 <template>
   <div class="container">
     <div class="row">
-      <div class="col s4">
-        <div v-for="i in array" :key="i" class="card blue-grey darken-1">
-          <div class="card-content white-text">
-            <span class="trendName card-title">#BalanceTonYoutubeur</span>
-            <span class="tendance">
-              <i class="material-icons">trending_up</i>
-              <p>+12.32%</p>
-            </span>
+      <div class="topics col-4">
+        <div v-for="(topic, index) in trendingTopics" :key="index"  class="card bg-light mb-3">
+          <div class="card-body" @click="browseHashtag(topic.name)">
+            <h5 class="card-title">{{ topic.name }}<span class="text-muted" v-if="topic.tweet_volume"> - {{ topic.tweet_volume }}</span></h5>
           </div>
         </div>
       </div>
-      <div class="col s8">
-        <div v-for="i in arr" :key="i" class="card blue-grey darken-1">
-          <div class="card-content white-text">
-            <span class="card-title">@UnRandomDébilos - 17:28:54</span>
-            <p>Abusé le comportement de Norman, il faudrait le tuer je pense jpp...</p>
-          </div>
-        </div>
+      <div class="col-8">
+        <Tweets :hashtag="hashtag" />
       </div>
     </div>
   </div>
@@ -26,28 +17,36 @@
 
 <script>
 import axios from 'axios'
+import Tweets from '@/components/Tweets'
 export default {
   name: 'Dashboard',
-  data () {
+  data() {
     return {
-      array: [1, 2, 3, 4, 5, 6, 7, 8],
-      arr: [1, 2, 3, 4, 5, 6, 7]
+      trendingTopics: null,
+      hashtag: null
     }
+  },
+  components: {
+    Tweets
+  },
+  methods: {
+    browseHashtag(hashtag) {
+      this.hashtag = hashtag
+    }
+  },
+  mounted(){
+    axios
+      .get('http://localhost:3000/trending')
+      .then(response => this.trendingTopics = response.data[0].trends)
+
   }
 }
+
 </script>
 
 <style>
-.tendance i{
-  position: absolute;
-  right: 10px;
-  top: 10px;
-  font-size: 3em;
-}
-
-.tendance p{
-  position: absolute;
-  right: 10px;
-  top: 48px;;
+.topics .card {
+  cursor: pointer;
+  height: 4em;
 }
 </style>
